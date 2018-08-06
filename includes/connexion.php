f1f1eb29bce4bf3683411da8f34a74729902a18c
stephane23 <40633363+stephane23@users.noreply.github.com>
2018-08-06 16:11:09 +0200

    Update connexion.php

--- includes/connexion.php
+++ includes/connexion.php
@@ -1,7 +1,7 @@
 <?php 
 try {
 
-    $bdd = new PDO('mysql:host=localhost;dbname=mini_chat_slyt;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host='.(getenv('MYSQL_HOST') ?: 'localhost').';dbname=mini_chat_slyt;charset=utf8', 'root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }catch(Exception $e){
 
